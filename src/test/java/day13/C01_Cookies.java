f4ca81db91f9599aaafbeb1b8f0e83f66ddcc908
FatihKamilAltun <fkaltun70@gmail.com>
2022-09-24 16:03:30 +0300

    24/09/2022

--- src/test/java/day13/C01_Cookies.java
+++ src/test/java/day13/C01_Cookies.java
@@ -28,34 +28,88 @@ public class C01_Cookies extends TestBaseBeforeAfter {
         }
 
         // 3- Sayfadaki cookies sayisinin 5’den buyuk oldugunu test edin
-        Assert.assertTrue(cookiesSet.size()>5);
+        Assert.assertTrue(cookiesSet.size() > 5);
 
         // 4- ismi i18n-prefs olan cookie degerinin USD oldugunu test edin
-        for (Cookie each:cookiesSet
-             ) {
-            if (each.getName().equals("i18n-prefs")){
-                Assert.assertEquals("USD",each.getValue());
+        for (Cookie each : cookiesSet
+        ) {
+            if (each.getName().equals("i18n-prefs")) {
+                Assert.assertEquals("USD", each.getValue());
             }
         }
 
         // 5-ismi “en sevdigim cookie” ve degeri “cikolatali” olan bir cookie  olusturun ve sayfaya ekleyin
-        Cookie newCookie=new Cookie("en sevdigim cookie","cikolatali");
+        Cookie newCookie = new Cookie("en sevdigim cookie", "cikolatali");
         driver.manage().addCookie(newCookie);
-        cookiesSet=driver.manage().getCookies();
-        for (Cookie each:cookiesSet
-             ) {
+        cookiesSet = driver.manage().getCookies();
+        for (Cookie each : cookiesSet
+        ) {
             System.out.println(each.getName());
         }
         // 6- eklediginiz cookie’nin sayfaya eklendigini test edin
         Assert.assertTrue(cookiesSet.contains(newCookie));
 
         // 7- ismi skin olan cookie’yi silin ve silindigini test edin
-        String skinCookie=driver.manage().getCookieNamed("skin").getName();
+        String skinCookie = driver.manage().getCookieNamed("skin").getName();
         Assert.assertFalse(cookiesSet.contains(skinCookie));
 
         // 8- tum cookie’leri silin ve silindigini test edin
         driver.manage().deleteAllCookies();
-        cookiesSet=driver.manage().getCookies();
+        cookiesSet = driver.manage().getCookies();
         Assert.assertTrue(cookiesSet.isEmpty());
+    }
+
+    @Test
+    public void test02() {
+        // 1- Amazon anasayfaya gidin
+        driver.get("https://amazon.com");
+        // 2- tum cookie’leri listeleyin
+        int count = 1;
+        Set<Cookie> cookieSet = driver.manage().getCookies();
+        System.out.println(cookieSet);
+        for (Cookie each : cookieSet
+        ) {
+            System.out.println(count + ".nci cookie : " + each);
+            System.out.println("name : " + each.getName());
+            System.out.println("value : " + each.getValue());
+            count++;
+        }
+        // 3- Sayfadaki cookies sayisinin 5’den buyuk oldugunu test edin
+        int cookiesSize = cookieSet.size();
+        Assert.assertTrue(cookiesSize > 5);
+
+        // 4- ismi i18n-prefs olan cookie degerinin USD oldugunu test edin
+        for (Cookie each : cookieSet
+        ) {
+            if (each.getName().equals("i18n-prefs")) {
+                Assert.assertEquals("USD", each.getValue());
+            }
+        }
+
+        // 5-ismi “en sevdigim cookie” ve degeri “cikolatali” olan bir cookie
+        // olusturun ve sayfaya ekleyin
+        Cookie newCookie=new Cookie("en sevdigimcookie","cikolatali");
+        driver.manage().addCookie(newCookie);
+        cookieSet=driver.manage().getCookies();
+        count=1;
+        for (Cookie each:cookieSet
+             ) {
+            System.out.println(count+".nci cookie : " + each);
+            count++;
+        }
+
+        // 6- eklediginiz cookie’nin sayfaya eklendigini test edin
+        Assert.assertTrue(cookieSet.contains(newCookie));
+
+        // 7- ismi skin olan cookie’yi silin ve silindigini test edin
+        driver.manage().deleteCookieNamed("skin");
+        cookieSet=driver.manage().getCookies();
+        Assert.assertFalse(cookieSet.contains("skin"));
+
+        // 8- tum cookie’leri silin ve silindigini test edin
+        driver.manage().deleteAllCookies();
+        cookieSet=driver.manage().getCookies();
+        Assert.assertTrue(cookieSet.isEmpty());
+
     }
 }
--- src/test/java/day13/C02_WebTables.java
+++ src/test/java/day13/C02_WebTables.java
@@ -12,40 +12,63 @@ import java.util.List;
 public class C02_WebTables extends TestBaseBeforeAfter {
     @Test
     public void test01() {
-        // Bir class oluşturun : C02_WebTables
-        // login( ) metodun oluşturun ve oturum açın.
-        // https://www.hotelmycamp.com/admin/HotelRoomAdmin adresine gidin
-        login();
-        // Username : manager
-        // Password : Manager1!
+        // ● Bir class oluşturun : C02_WebTables
+        // ● login( ) metodun oluşturun ve oturum açın.
+        girisYap();
 
+        // ● table( ) metodu oluşturun
+        // ○ Tüm table body’sinin boyutunu(sutun sayisi) bulun. /tbody
+        List<WebElement> sutunBasliklari = driver.findElements(By.xpath("//thead//tr[1]//th"));
+        System.out.println("Sutun sayisi : " + sutunBasliklari.size());
+
+        // ○ Table’daki tum body’I ve başlıkları(headers) konsolda yazdırın.
+
+        // Tum body'i bir string olarak yazdirmak isterseniz body elementini locate ederk getText() ile yazdırılabilir
+        WebElement tumBody = driver.findElement(By.xpath("//tbody"));
+        System.out.println(tumBody.getText());
+
+        // ● printRows( ) metodu oluşturun //tr
+        // ○ table body’sinde bulunan toplam satir(row) sayısını bulun.
+        List<WebElement> satirlarListesi = driver.findElements(By.xpath("//tbody//tr"));
+        System.out.println("Satir sayisi : " + satirlarListesi.size());
+
+        // ○ Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
+        for (WebElement each : satirlarListesi
+        ) {
+            System.out.println(each.getText());
+        }
+
+        // ○ 4.satirdaki(row) elementleri konsolda yazdırın
+        List<WebElement> cellList = driver.findElements(By.xpath("//tbody//td"));
+        for (WebElement each : cellList
+        ) {
+            System.out.println(each.getText());
+        }
+
+        // Email basligindaki tum elementleri(sutun) konsolda yazdırın
+        List<WebElement> basliklarListesi=driver.findElements(By.xpath("//thead//tr[1]//th"));
+        int emailSutunNo=0;
+        for (int i = 0; i < basliklarListesi.size() ; i++) {
+            if (basliklarListesi.get(i).getText().equals("Email")){
+                emailSutunNo=i;
+            }
+        }
+        List<WebElement> emailSutunListesi=driver.findElements(By.xpath("//tbody//td["+(emailSutunNo+1)+"]"));
+        for (WebElement each:emailSutunListesi) {
+            System.out.println(each.getText());
+        }
+    }
+
+    private void girisYap() {
+        driver.get("https://www.hotelmycamp.com");
+        driver.findElement(By.xpath("//*[text()='Log in']")).click();
+        WebElement loginBox = driver.findElement(By.xpath("//input[@class='form-control required']"));
+        actions.click(loginBox).
+                sendKeys("manager" + Keys.TAB).
+                sendKeys("Manager1!").
+                perform();
+        driver.findElement(By.xpath("//input[@id='btnSubmit']")).click();
     }
 
 
-    private void table() {
-        // Tüm table body’sinin boyutunu(sutun sayisi) bulun.
-
-        /*
-
-         */
-
-
-        List<WebElement> sutunSayisi=driver.findElements(By.xpath("//thead//tr//th"));
-        System.out.println("Sutun sayisi : " + sutunSayisi.size());
-        // Table’daki tum body’I ve başlıkları(headers) konsolda yazdırın.
-        // printRows( ) metodu oluşturun //tr
-        // table body’sinde bulunan toplam satir(row) sayısını bulun.
-        // Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
-        // 4.satirdaki(row) elementleri konsolda yazdırın.
-    }
-
-    private void login() {
-        driver.get("https://www.hotelmycamp.com/admin/HotelRoomAdmin");
-        // driver.findElement(By.xpath("//*[@id='UserName']")).click();
-        Actions actions = new Actions(driver);
-        WebElement userName = driver.findElement(By.xpath("//*[@id='UserName']"));
-        actions.click(userName).sendKeys("manager").
-                sendKeys(Keys.TAB).sendKeys("Manager1!").sendKeys(Keys.ENTER).perform();
-
-    }
 }
